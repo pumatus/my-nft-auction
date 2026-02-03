2d83d2dfcf0ece47657c938e7450339cabd21c1e
何鸿达 <1009441951@qq.com>
2026-02-03 10:45:29 +0800

    doc: 为NFT 合约文件添加注释

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,23 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-
+// 引入openzeppelin的721实现和权限控制
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // 铸造合约 作为拍卖的对象
 contract MyNFT is ERC721, Ownable {
+    // 用于记录下一个铸造的token ID ，确保ID的唯一性
     uint256 private _nextTokenId;
 
+    //初始化名称和代币符号 owner设置为部署者
     constructor() ERC721("AuctionNFT", "ANFT") Ownable(msg.sender) {}
 
+    //铸造nft 合约所有者调用
     function mint(address to) public onlyOwner returns (uint256) {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
+        uint256 tokenId = _nextTokenId++; // ID 自增
+        _safeMint(to, tokenId); // 安全铸造 确保地址能够接收nft
         return tokenId;
     }
 
+    // 销毁nft 合约所有者调用
     function burn(uint256 id) external onlyOwner {
+        // 确保当前持有该nft
         require(msg.sender == ownerOf(id), "no owner");
-        _burn(id);
+        _burn(id); // 销毁调用
     }
 }
--- test/NFTAuctionMarket.ts
+++ test/NFTAuctionMarket.ts
@@ -1,28 +1,28 @@
-// import { expect } from "chai";
-// import hre from "hardhat";
+import { expect } from "chai";
+import hre from "hardhat";
 
-// describe("NFT 拍卖市场功能测试", function () {
-//   let market: any;
-//   let nft: any;
-//   let deployer: any;
+describe("NFT 拍卖市场功能测试", function () {
+  let market: any;
+  let nft: any;
+  let deployer: any;
 
-//   beforeEach(async function () {
-//     // 使用 as any 避开 TypeScript 对 hre 扩展属性的检查
-//     const { ethers, deployments } = hre as any; 
+  beforeEach(async function () {
+    // 使用 as any 避开 TypeScript 对 hre 扩展属性的检查
+    const { ethers, deployments } = hre as any; 
 
-//     // 运行部署脚本
-//     await deployments.fixture(["NFTMarket"]);
+    // 运行部署脚本
+    await deployments.fixture(["NFTMarket"]);
 
-//     // 获取合约
-//     const marketDeployment = await deployments.get("NFTAuctionMarket");
-//     const nftDeployment = await deployments.get("MyNFT");
+    // 获取合约
+    const marketDeployment = await deployments.get("NFTAuctionMarket");
+    const nftDeployment = await deployments.get("MyNFT");
 
-//     [deployer] = await ethers.getSigners();
-//     market = await ethers.getContractAt("NFTAuctionMarket", marketDeployment.address);
-//     nft = await ethers.getContractAt("MyNFT", nftDeployment.address);
-//   });
+    [deployer] = await ethers.getSigners();
+    market = await ethers.getContractAt("NFTAuctionMarket", marketDeployment.address);
+    nft = await ethers.getContractAt("MyNFT", nftDeployment.address);
+  });
 
-//   it("应该能正常运行", async function () {
-//     expect(market.target).to.properAddress;
-//   });
-// });
+  it("应该能正常运行", async function () {
+    expect(market.target).to.properAddress;
+  });
+});
